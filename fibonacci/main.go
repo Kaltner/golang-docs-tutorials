72c4650031b21d0e7fba314c9233bb7faabbc6a1
Danilo Kaltner <danilo.kaltner@gmail.com>
2020-10-23 08:26:11 -0700

    A much more elegant solution

--- fibonacci/main.go
+++ fibonacci/main.go
@@ -3,17 +3,11 @@ package main
 import "fmt"
 
 func fibonacci() func() int {
-	sequence := []int{0, 1}
-	i := 0
+	x := 1
+	y := 0
 	return func() int {
-		if i < 2 {
-			i++
-			return sequence[i-1]
-		}
-		l := len(sequence)
-
-		sequence = append(sequence, sequence[l-1]+sequence[l-2])
-		return sequence[l] // Will return the latest value, since sequence is 0 indexed
+		x, y = y, x+y
+		return x
 	}
 }
 
