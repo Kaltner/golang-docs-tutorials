3b7d1626b266ef20b65ed119afa98a625f016c17
Danilo Kaltner <danilo.kaltner@gmail.com>
2020-11-16 09:02:50 -0800

    Created the Same function

--- binary-trees/main.go
+++ binary-trees/main.go
@@ -26,35 +26,57 @@ func walk(t *tree.Tree, ch chan int) {
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
-	// ch1 := make(chan int, 10)
-	// ch2 := make(chan int, 10)
+	ch1 := make(chan int)
+	ch2 := make(chan int)
 
-	// for n := range <-ch1 {
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
 
-	// }
+	result1, result2 := createResults(ch1), createResults(ch2)
+
+	for _, n1 := range result1 {
+		foundValue := false
+		for _, n2 := range result2 {
+			if n1 == n2 {
+				foundValue = true
+				break
+			}
+		}
+		if !foundValue {
+			return false
+		}
+	}
 	return true
 }
 
+func createResults(ch chan int) []int {
+	result := make([]int, 0)
+	for i := range ch {
+		result = append(result, i)
+	}
+	return result
+}
+
 func main() {
-	ch1 := make(chan int)
+	// ch1 := make(chan int)
 	t1 := tree.New(1)
 
-	go Walk(t1, ch1)
+	// go Walk(t1, ch1)
 
 	// ch2 := make(chan int, 10)
-	// t2 := tree.New(255)
+	t2 := tree.New(255)
 
 	// go Walk(t2, ch2)
 
-	nums1 := make([]int, 0)
+	// nums1 := make([]int, 0)
 	// for i := 0; i < 10; i++ {
 	// 	nums1[i] = <-ch1
 	// }
-	for val := range ch1 {
-		nums1 = append(nums1, val)
-	}
+	// for val := range ch1 {
+	// nums1 = append(nums1, val)
+	// }
 
-	fmt.Println(nums1)
+	// fmt.Println(nums1)
 
 	// nums2 := make([]int, 10)
 	// for i := 0; i < 10; i++ {
@@ -62,6 +84,6 @@ func main() {
 	// }
 	// fmt.Println(nums2)
 
-	// fmt.Println(Same(t1, t1))
-	// fmt.Println(Same(t1, t2))
+	fmt.Println(Same(t1, t1))
+	fmt.Println(Same(t1, t2))
 }
